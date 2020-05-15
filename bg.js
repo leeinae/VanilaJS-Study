ed304132e0d3c59898e572003ebdfcb11e9e751f
inaeLee <inae9709@hanmail.net>
2020-05-16 03:23:19 +0900

    Add random background img

---
+++ bg.js
@@ -0,0 +1,28 @@
+const body = document.querySelector("body");
+
+const IMG_NUMBER = 3;
+
+function handleImgLoad() {
+    console.log("finish"); 
+}
+
+function paintImage(imageNum) {
+    const image = new Image();
+    image.src = `Images/image(${imageNum + 1}).jpg`
+    image.classList.add("bgImg");
+    body.appendChild(image);
+    image.addEventListener("loaded", handleImgLoad);
+}
+
+function getRandom() {
+    const number = Math.floor(Math.random() * 6);
+    console.log(number);
+    return number;
+}
+
+function init() {
+    const randomNumber = getRandom();
+    paintImage(randomNumber);
+}
+
+init();
