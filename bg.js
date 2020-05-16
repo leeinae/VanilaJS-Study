c0ad7c4b5205875c7b6e98777248fc0efe9defba
inaeLee <inae9709@hanmail.net>
2020-05-16 21:00:24 +0900

    Add next, prev btn slide feature

--- bg.js
+++ bg.js
@@ -1,22 +1,27 @@
-const body = document.querySelector("#silder");
+const body = document.querySelector("#slider");
 const IMG_COUNT = 6;
 const SHOWING = "bg-showing";
+const nextBtn = document.querySelector("#next");
+const prevBtn = document.querySelector("#prev");
 
-function silde() {
-    const firstSilde = document.querySelector(".bgImg:first-child");
-    const currentSilde = document.querySelector(`.${SHOWING}`);
+function slide(event) {
+    const id = event.target.id;
+    const firstSlide = document.querySelector(".bgImg:first-child");
+    const lastSlide = document.querySelector(".bgImg:last-child");
+    const currentSlide = document.querySelector(`.${SHOWING}`);
 
-    if(currentSilde) {
-        currentSilde.classList.remove(SHOWING);
-        const nextSilde = currentSilde.nextElementSibling;
-
-        if(nextSilde) {
-            nextSilde.classList.add(SHOWING);
+    if(currentSlide) {
+        currentSlide.classList.remove(SHOWING);
+        
+        if(id == 'next') {
+            const nextSlide = currentSlide.nextElementSibling ? currentSlide.nextElementSibling : firstSlide;
+            nextSlide.classList.add(SHOWING);
         } else {
-            firstSilde.classList.add(SHOWING);
+            const prevSlide = currentSlide.previousElementSibling ? currentSlide.previousElementSibling : lastSlide;
+            prevSlide.classList.add(SHOWING);
         }
     } else {
-        firstSilde.classList.add(SHOWING);
+        firstSlide.classList.add(SHOWING);
     }
 }
 
@@ -26,16 +31,19 @@ function paintImage() {
         
         image.src = `Images/image(${i}).jpg`
         image.classList.add("bgImg");
+
         if(i==1) {
             image.classList.add("bg-showing");
         }
+
         body.appendChild(image);
     }
 }
 
 function init() {
     paintImage();
+    nextBtn.addEventListener('click', slide);
+    prevBtn.addEventListener('click', slide);
 }
 
 init();
-setInterval(silde, 5000);
